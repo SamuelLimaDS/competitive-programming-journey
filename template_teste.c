66ff1215be92903a3724c2651a8aa9c270aad5ec
SamuelLimaDS <samuellimadossantos01@gmail.com>
2026-06-22 23:28:50 -0300

    template program

---
+++ template_teste.c
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
